5e61fe665fd6125919b8f9f1866f4bc3be1da0ca
David Sanchez <e.david.sanchez.b@gmail.com>
2021-02-20 18:19:36 -0500

    Implementando Reflect en MiniVue

--- miniVue.js
+++ miniVue.js
@@ -5,11 +5,14 @@ class miniVueReactive {
         // Destino
         this.$data = new Proxy(this.origen, {
             get(target, name) {
-                if(name in target) {
-                    return target[name];
+                if(Reflect.has(target, name)) {
+                    return Reflect.get(target, name);
                 }
                 console.warn("la propiedad", name, "no existe.");
                 return "";
+            },
+            set(target, name, value) {
+                Reflect.set(target, name, value);
             }
         });
     }
@@ -17,11 +20,21 @@ class miniVueReactive {
         document.querySelectorAll("*[p-text]").forEach(el => {
             this.pText(el, this.$data, el.getAttribute("p-text"))
         });
+        document.querySelectorAll("*[p-model]").forEach(
+            el => {
+                const name = el.getAttribute("p-model");
+                this.pModel(el, this.$data, name);
+                el.addEventListener("input", () => {
+                    Reflect.set(target, name, value);
+                });
+        });
     }
     pText(el, target, name) {
-        el.innerText = target[name];
+        el.innerText = Reflect.get(target, name);
     }
-    pModel() {}
+    pModel(el, target, name) {
+        el.value = Reflect.get(target, name);
+    }
 }
 
 var miniVue = {
