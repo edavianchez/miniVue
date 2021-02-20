2a007f7f60cd2bf360effe534f387a5eeec20c1c
David Sanchez <e.david.sanchez.b@gmail.com>
2021-02-19 22:31:08 -0500

    Implementando un Proxy en MiniVue

--- miniVue.js
+++ miniVue.js
@@ -1,10 +1,21 @@
 class miniVueReactive {
     constructor(options){
+        // Origen
         this.origen = options.data();
+        // Destino
+        this.$data = new Proxy(this.origen, {
+            get(target, name) {
+                if(name in target) {
+                    return target[name];
+                }
+                console.warn("la propiedad", name, "no existe.");
+                return "";
+            }
+        });
     }
     mount(){
         document.querySelectorAll("*[p-text]").forEach(el => {
-            this.pText(el, this.origen, el.getAttribute("p-text"))
+            this.pText(el, this.$data, el.getAttribute("p-text"))
         });
     }
     pText(el, target, name) {
