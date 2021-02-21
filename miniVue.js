0b45d5899762d7b9aa603a19ada715d330f98d08
David Sanchez <e.david.sanchez.b@gmail.com>
2021-02-20 22:24:20 -0500

    Implementando effect, track y trigger en MiniVue

--- miniVue.js
+++ miniVue.js
@@ -1,44 +1,82 @@
 class miniVueReactive {
-    constructor(options){
-        // Origen
-        this.origen = options.data();
-        // Destino
-        this.$data = new Proxy(this.origen, {
-            get(target, name) {
-                if(Reflect.has(target, name)) {
-                    return Reflect.get(target, name);
-                }
-                console.warn("la propiedad", name, "no existe.");
-                return "";
-            },
-            set(target, name, value) {
-                Reflect.set(target, name, value);
-            }
+  // Dependencias
+  deps = new Map();
+
+  constructor(options) {
+    // Origen
+    this.origen = options.data();
+
+    const self = this;
+
+    // Destino
+    this.$data = new Proxy(this.origen, {
+      get(target, name) {
+        if (Reflect.has(target, name)) {
+          self.track(target, name);
+          return Reflect.get(target, name);
+        }
+        console.warn("la propiedad", name, "no existe.");
+        return "";
+      },
+      set(target, name, value) {
+        Reflect.set(target, name, value);
+        self.trigger(name);
+      },
+    });
+  }
+
+  track(target, name) {
+    if (!this.deps.has(name)) {
+      const effect = () => {
+        document.querySelectorAll(`*[p-text=${name}]`).forEach((el) => {
+          this.pText(el, target, name);
         });
-    }
-    mount(){
-        document.querySelectorAll("*[p-text]").forEach(el => {
-            this.pText(el, this.$data, el.getAttribute("p-text"))
+        document.querySelectorAll(`*[p-model=${name}]`).forEach((el) => {
+          this.pModel(el, target, name);
         });
-        document.querySelectorAll("*[p-model]").forEach(
-            el => {
-                const name = el.getAttribute("p-model");
-                this.pModel(el, this.$data, name);
-                el.addEventListener("input", () => {
-                    Reflect.set(target, name, value);
-                });
+        document.querySelectorAll("*[p-bind]").forEach(el => {
+          const [attr, name] = el.getAttribute("p-bind").match(/(\w+)/g);
+          this.pBind(el, target, name, attr);
         });
+      };
+      this.deps.set(name, effect);
     }
-    pText(el, target, name) {
-        el.innerText = Reflect.get(target, name);
-    }
-    pModel(el, target, name) {
-        el.value = Reflect.get(target, name);
-    }
+  }
+
+  trigger(name) {
+    const effect = this.deps.get(name);
+    effect();
+  }
+
+  mount() {
+    document.querySelectorAll("*[p-text]").forEach((el) => {
+      this.pText(el, this.$data, el.getAttribute("p-text"));
+    });
+    document.querySelectorAll("*[p-model]").forEach((el) => {
+      const name = el.getAttribute("p-model");
+      this.pModel(el, this.$data, name);
+      el.addEventListener("input", () => {
+        Reflect.set(this.$data, name, el.value);
+      });
+    });
+    document.querySelectorAll("*[p-bind]").forEach(el => {
+      const [attr, name] = el.getAttribute("p-bind").match(/(\w+)/g);
+      this.pBind(el, this.$data, name, attr);
+    });
+  }
+  pText(el, target, name) {
+    el.innerText = Reflect.get(target, name);
+  }
+  pModel(el, target, name) {
+    el.value = Reflect.get(target, name);
+  }
+  pBind(el, target, name, attr) {
+    el.setAttribute(attr, Reflect.get(target, name))
+  }
 }
 
 var miniVue = {
-    createApp(options) {
-        return new miniVueReactive(options);
-    }
+  createApp(options) {
+    return new miniVueReactive(options);
+  },
 };
